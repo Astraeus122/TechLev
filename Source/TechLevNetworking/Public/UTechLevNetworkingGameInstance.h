87a5372a4f834704579b1eeda1df23a12e16f84d
Astraeus122 <ryanvaningenkal@gmail.com>
2024-06-27 16:00:14 +1200

    Start

---
+++ Source/TechLevNetworking/Public/UTechLevNetworkingGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "UTechLevNetworkingGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECHLEVNETWORKING_API UUTechLevNetworkingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TechLevNetworking/TechLevNetworking.Build.cs
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TechLevNetworking : ModuleRules
+{
+	public TechLevNetworking(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput",  "OnlineSubsystem",
+            "OnlineSubsystemUtils", "UMG" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+    }
+}
